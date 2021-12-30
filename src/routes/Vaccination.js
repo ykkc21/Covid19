375856904c96b0f0df20d4f433c2a323260a3c1a
ykkc21 <ykkc1595@gmail.com>
2021-12-31 00:08:29 +0900

    commit

--- src/routes/Vaccination.js
+++ src/routes/Vaccination.js
@@ -1,17 +1,45 @@
 import { useEffect, useState } from "react";
-import MapElement from "../components/Map/Map";
+// import MapElement from "../components/Map/Map";
 function Vaccination() {
   const { kakao } = window;
   const [loading, SetLoading] = useState(true);
 
   useEffect(() => {
     navigator.geolocation.watchPosition((pos) => {
-      console.log(pos.coords.latitude, pos.coords.longitude);
+      let latitude = pos.coords.latitude;
+      let longitude = pos.coords.longitude;
+      Maps.Draw(latitude, longitude);
     });
-    SetLoading(false);
   }, []);
+
+  const Maps = {
+    init() {
+      this.Draw();
+    },
+    Draw(lat, long) {
+      const container = document.getElementById("map");
+
+      const options = {
+        center: new kakao.maps.LatLng(lat, long),
+        level: 3,
+      };
+
+      var map = new kakao.maps.Map(container, options);
+
+      var markerPosition = new kakao.maps.LatLng(lat, long);
+
+      var marker = new kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      marker.setMap(map);
+    },
+  };
+
   return (
-    <div className="Map_wrap" style={{ width: "100%", height: "100vh" }}></div>
+    <div className="Map_wrap" style={{ width: "100%", height: "100vh" }}>
+      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+    </div>
   );
 }
 export default Vaccination;
