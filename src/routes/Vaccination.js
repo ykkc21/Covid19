8b59590c2b1698e9bf07b7a8105c73203d04936d
ykkc21 <ykkc1595@gmail.com>
2021-12-30 23:00:02 +0900

    commit

--- src/routes/Vaccination.js
+++ src/routes/Vaccination.js
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
 function Vaccination() {
-  return <h1>내 주변 예방접종센터</h1>;
+  const { kakao } = window;
+  const [loading, SetLoading] = useState(true);
+
+  useEffect(() => {
+    KakaoMap.init();
+    navigator.geolocation.watchPosition((pos) => {
+      let latitude = pos.coords.latitude;
+      let longitude = pos.coords.longitude;
+      KakaoMap.drawMap(latitude, longitude);
+      SetLoading(false);
+    });
+  }, []);
+
+  const KakaoMap = {
+    init() {},
+    drawMap(latitude, longitude) {
+      const container = document.getElementById("map");
+
+      var options = {
+        center: new kakao.maps.LatLng(latitude, longitude),
+        level: 3,
+      };
+
+      var map = new kakao.maps.Map(container, options);
+    },
+  };
+
+  return <>{loading ? <h1>...loading</h1> : <h1>Hi</h1>}</>;
 }
 export default Vaccination;
