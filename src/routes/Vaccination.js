2d525f6a4a304d3a1637b8e32d8996fc9f54f5eb
ykkc21 <ykkc1595@gmail.com>
2022-01-09 21:32:48 +0900

    commit

--- src/routes/Vaccination.js
+++ src/routes/Vaccination.js
@@ -34,7 +34,7 @@ function Vaccination() {
       var mapContainer = document.getElementById("map"), // 지도를 표시할 div
         mapOption = {
           center: new kakao.maps.LatLng(37.57613074143258, 126.97696604175948), // 지도의 중심좌표
-          level: 12, // 지도의 확대 레벨
+          level: 11, // 지도의 확대 레벨
         };
       var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
