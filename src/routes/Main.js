7537fb5bdc7378169689bc73d6c5a248efbc4978
ykkc21 <ykkc1595@gmail.com>
2022-01-13 23:14:33 +0900

    commit

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -6,14 +6,19 @@ function Main() {
   return (
     <div className="wrap">
       <div className="Menu_wrap col-lg-12">
-        <div className="container">
+        <div className="Menu_Box col-lg-10">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+        {/* <div className="container">
           <div className="MenuBox">
             <h1>COVID-19</h1>
             <Box text="Korea" />
             <Box text="World" />
             <Box text="Vaccination" />
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
